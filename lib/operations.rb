8b4ac85a05692ecb02dc58dc8858c51ab9e44b8d
Rianna Castro <riannaxcastro@gmail.com>
2021-02-25 12:35:29 -1000

    commit tree ff7cf40089b153c35966ed46a8a1c957ea3720c5

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,35 @@
 def unsafe?(speed)
 
+    if speed > 60 
+        return true
+        
+
+    end
+    
+    if speed < 40
+        return true
+    end
+
+    if speed < 60 
+        return false
+    end
+
+    if speed > 40
+        return false
+    end
+
+
+
 end
 
 
 
 def not_safe?(speed)
-	
+
+	  speed < 40 || speed > 60 ? true : false
+
+    
+    
 end
 	
 
